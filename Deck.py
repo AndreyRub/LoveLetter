4eba961d0d99d98c9dfc516f1fcf028265ccc4df
Andrey <andrey.rubshtein@gmail.com>
2018-06-26 02:19:58 +0300

    Improved the style a bit more

--- Deck.py
+++ Deck.py
@@ -17,48 +17,43 @@ class Deck:
 # 		shuffle:	shuffles the "cards" list
 # 		deal_card:	pop one card from the deck and return it.
 
+    def get_num_of_cards_per_type(self):
+        num_of_cards_per_type = {
+            1: 5, 2: 2, 3: 2, 4: 2, 5: 2, 6: 1, 7: 1, 8: 1
+        }
+        return num_of_cards_per_type
+
     def get_descriptions(self,cards_style="Japanese"):
         if cards_style=="Japanese":
             names = ['Guard','Courtier','Diplomat','Shugenja','Hatamoto','Manipulator','Sensei','Princess']
         elif cards_style=="Batman":
             names = ['Batman','Catwoman','Bane','Robin','Poison Ivy','Two Face','Harley Quinn','Joker']
         elif cards_style=="Classic":
-            names = ['Guard','Priest','Baron','Handmaiden','Prince','King','Countess','Pricess']
+            names = ['Guard','Priest','Baron','Handmaiden','Prince','King','Countess','Princess']
+        elif cards_style=="Coup":
+            names = ['Guard', 'Captain', 'Duke', 'Ambassador', 'Inquisitor', 'Captain', 'Contessa', 'Princess']
+        num_of_cards_per_type = self.get_num_of_cards_per_type()
         descriptions = [
-                    '%s(5) - Guess a player\'s hand' % names[0],
-                    '%s(2) - Look at a hand' % names[1],
-                    '%s(2) - Compare hands' % names[2],
-                    '%s(2) - Protection until your next turn' % names[3],
-                    '%s(2) - One player discards his or her hand' % names[4],
-                    '%s(1) - Trade hands' % names[5],
-                    '%s(1) - Discard if caught with %s or %s' % (names[6],names[4],names[5]),
-                    '%s(1) - Lose if discarded' %names[7]]
-
-
+                    '%s(%d) - Guess a player\'s hand' % (names[0],num_of_cards_per_type[1]) ,
+                    '%s(%d) - Look at a hand' % (names[1],num_of_cards_per_type[2]),
+                    '%s(%d) - Compare hands' % (names[2],num_of_cards_per_type[3]),
+                    '%s(%d) - Protection until your next turn' % (names[3],num_of_cards_per_type[4]),
+                    '%s(%d) - One player discards his or her hand' % (names[4],num_of_cards_per_type[5]),
+                    '%s(%d) - Trade hands' % (names[5],num_of_cards_per_type[6]),
+                    '%s(%d) - Discard if caught with %s or %s' % (names[6],num_of_cards_per_type[7],names[4],names[5]),
+                    '%s(%d) - Lose if discarded' % (names[7],num_of_cards_per_type[8])]
         return descriptions
 
     def __init__(self, num_of_unused_cards=1,cards_style = "Japanese"):
         self.num_of_unused_cards = num_of_unused_cards
-
         # Create cards as per the game (5 "1"s, 2 "2","3","4","5"s, 1 "6","7","8", etc.)
+        num_of_cards_per_type = self.get_num_of_cards_per_type()
         descriptions = self.get_descriptions(cards_style)
-        cards =[Card(1,descriptions[0]),
-                Card(1,descriptions[0]),
-                Card(1,descriptions[0]),
-                Card(1,descriptions[0]),
-                Card(1,descriptions[0]),
-                Card(2,descriptions[1]),
-                Card(2,descriptions[1]),
-                Card(3,descriptions[2]),
-                Card(3,descriptions[2]),
-                Card(4,descriptions[3]),
-                Card(4,descriptions[3]),
-                Card(5,descriptions[4]),
-                Card(5,descriptions[4]),
-                Card(6,descriptions[5]),
-                Card(7,descriptions[6]),
-                Card(8,descriptions[7]),
-                ]
+        cards = []
+        for card_type in sorted(num_of_cards_per_type):
+            num_of_cards = num_of_cards_per_type[card_type]
+            for j in range(0,num_of_cards):
+                cards.append( Card(card_type,descriptions[card_type-1]) )
 
         self.descriptions = descriptions
         self.cards = cards
--- Main.py
+++ Main.py
@@ -5,3 +5,4 @@ from Game import Game
 game = Game(3,cards_style = "Classic")
 #game = Game(2,cards_style = "Batman")
 game.play()
+winner = game.decide_winner()
