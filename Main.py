a48630014dbd8eecd71bd613560bc8c2b656fdc7
Andrey <andrey@sizer.me>
2018-06-26 19:27:27 +0300

    Merged love_letter.py to Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,18 @@
-import math
-import random
-from Game import Game
-from Player import Player
 from InputHuman import InputHuman
 
-player1 = InputHuman()
-player2 = InputHuman()
-players = [player1,player2]
-game = Game(players,cards_style = "Classic")
-game.play()
+from Game import Game
+from InputHuman import InputHuman
+num_of_players = 4
 
+human_players = [InputHuman("Player " + str(k)) for k in range(1,num_of_players+1)]
+
+# Set all players as Human
+players_list = [k for k in human_players]
+
+game = Game(players_list,cards_style="Classic")
+winners = game.play()
+if len(winners) == 1:
+    print(f"And the winner is: {winners[0].get_name()}!")
+else:
+    strs = ", ".join([w.get_name() for w in winners])
+    print(f"And the winners are: {strs}!")
